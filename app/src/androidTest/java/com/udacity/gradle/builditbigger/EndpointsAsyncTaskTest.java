03dc11c324d98e24014012b7004035ff355897bb
Anky <anky_au@hotmail.com>
2017-01-10 16:51:09 +1000

    Rebuild the test without overwriting AsyncTask

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,20 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.os.AsyncTask;
 import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 import android.text.TextUtils;
 
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.udacity.gradle.backend.myApi.MyApi;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.Assert.assertNotNull;
@@ -26,48 +18,13 @@ import static junit.framework.TestCase.fail;
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class EndpointsAsyncTaskTest {
-
-    private static MyApi myApiService = null;
+public class EndpointsAsyncTaskTest implements OnTaskCompleted {
 
     @Test
     public void test() throws Throwable {
-        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... voids) {
-                if (myApiService == null) {// Only do this once
-
-                    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                            new AndroidJsonFactory(), null)
-                            // options for running against local devappserver
-                            // - 10.0.2.2 is localhost's IP address in Android emulator
-                            // - turn off compression when running against local devappserver
-                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                                @Override
-                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                    abstractGoogleClientRequest.setDisableGZipContent(true);
-                                }
-                            });
-                    // end options for devappserver
-
-                    myApiService = builder.build();
-                }
-
-                try {
-                    return myApiService.fetchJokes().execute().getData();
-                } catch (IOException e) {
-                    return e.getMessage();
-                }
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                super.onPostExecute(result);
-            }
-        };
 
         try {
+            EndpointsAsyncTask task = new EndpointsAsyncTask(this);
             task.execute();
             String joke = task.get(30, TimeUnit.SECONDS);
             assertNotNull(joke);
@@ -75,5 +32,10 @@ public class EndpointsAsyncTaskTest {
         } catch (Exception e) {
             fail("Timed out");
         }
+    }
+
+    @Override
+    public void onTaskCompleted(String result) {
+
     }
 }
